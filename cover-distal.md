30a43b9dd2f30cd7125bc68c01c14d3d0320dc36
Kellie H. Mccullan <kmccullan@holosphere.gq>
2021-08-10 11:24:53 +0200

    added distal's cover

---
+++ cover-distal.md
@@ -0,0 +1,9 @@
+# Dr. Michel Combes Application for Head of Hardware
+
+
+I have an extensive background in leading team in small startup setup
+(Lemoptix, SoftComputing, Spatial Photonics, Reflectivity, Orient Direct, APT technologies)
+as well as larger corporation such as Intel, Intersil, Infineon, Texas Instruments...
+That is why I beleive I would be a great asset for leading the Distalmotion Engineering team.
+
+I will be available via phone: +41 76 760 9400.
--- publish.sh
+++ publish.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -e # halt script on error
+YY=$(date +%y)
 
 echo 'Publishing on IPFS...'
 bundle exec jekyll build
 #bundle exec htmlproof ./_site --verbose
 ipfs add -n -Q -r _site/ --cid-base=base58btc
-qm=$(ipfs add -Q -w -r _site/ --cid-base=base58btc)
-wrapid $qm resume$YY
-echo http://127.0.0.1:8080/ipfs/$qm/resume$YY
-
+qm=$(ipfs add -Q -r _site/ -cid-version=0)
+qmemptyd=$(ipfs object new unixfs-dir)
+wrap=$(ipfs object patch add-link $qmemptyd resume$YY $qm)
+#wrapid $qm resume$YY
+echo http://127.0.0.1:8080/ipfs/$wrap/resume$YY
+echo $wrap | base36 -d
 # post resume on IPFS network ...
-YY=$(date +%y)
 if ipfs files stat /my/documents/application/ 1>/dev/null; then
- ipfs file rm /my/documents/application/resume$YY
+ ipfs files rm -r /my/documents/application/resume$YY
 else
  ipfs files mkdir -p /my/documents/application/
 fi
 ipfs files cp /ipfs/$qm  /my/documents/application/resume$YY
-mt=$(mpublish /my/documents/application)
+#mt=$(mpublish /my/documents/application)
